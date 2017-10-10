78b5a46012c42df5d56c483e9e85bfe576b703ab
jb-matsunaga <jb.matsunaga@gmail.com>
2017-10-10 23:37:18 +0900

    add react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,23 +1,56 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
-const Page1 = () => {
+const Header = () => (
+  <div>
+    <p>Header</p>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/memo">Memo</Link></li>
+      <li><Link to="/profile">Profile</Link></li>
+    </ul>
+  </div>
+);
+
+const Home = () => {
   return (
     <div>
-      Page1
+      Home
     </div>
   )
 }
 
-const Page2 = () => {
+const Memo = () => {
   return (
     <div>
-      Page2
+      Memo
     </div>
+  )
+}
+
+const Profile = () => {
+  return (
+    <div>
+      Profile
+    </div>
+  )
+}
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route path="/memo" component={Memo} />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </BrowserRouter>
   )
 }
 
 render(
-  <Page1 />,
+  <Root />,
   document.getElementById('root')
 )
